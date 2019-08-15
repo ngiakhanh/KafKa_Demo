f07ebc950394284df3525b410218afec178accf3
WhiteK <gnguyen1@amaris.com>
2019-08-15 16:44:59 +0700

    Init

---
+++ Kafka_Consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace Kafka_Consumer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                Console.WriteLine("Hi, I am consumer.");
+                c.Subscribe("timemanagement_booking");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Leaving..");
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                    Console.WriteLine("Exited successfully!");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Kafka_Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Kafka_Producer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            void Handler(DeliveryReport<Null, string> r) =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                Console.WriteLine("Hi, I am producer.");
+
+                while (true)
+                {
+                    try
+                    {
+                        var mess = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(mess))
+                        {
+                            p.Produce("timemanagement_booking", new Message<Null, string> {Value = mess}, Handler);
+                            p.Flush(TimeSpan.FromSeconds(10));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
